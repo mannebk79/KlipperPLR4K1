77efa421273004e1d04cdc8bdbe26127152d6526
mannebk79 <60316272+mannebk79@users.noreply.github.com>
2026-04-14 20:14:41 +0200

    fix: enhance gcode_shell_command stability for K1 BusyBox
    Updated the Python wrapper to improve file descriptor handling and output parsing. This prevents "Timer too close" errors on the K1's fast SoC and ensures that complex G-code parameters (like directory paths in PLR) are correctly escaped and passed to the shell.

--- gcode_shell_command.py
+++ gcode_shell_command.py
@@ -1,8 +1,8 @@
 # Run a shell command via gcode
+# Optimized for Creality K1 Series / BusyBox
 #
 # Copyright (C) 2019  Eric Callahan <arksine.code@gmail.com>
-#
-# This file may be distributed under the terms of the GNU GPLv3 license.
+
 import os
 import shlex
 import subprocess
@@ -14,6 +14,7 @@ class ShellCommand:
         self.printer = config.get_printer()
         self.gcode = self.printer.lookup_object('gcode')
         cmd = config.get('command')
+        # Auf dem K1C als root ist expanduser wichtig, falls Scripte in ~/ liegen
         cmd = os.path.expanduser(cmd)
         self.command = shlex.split(cmd)
         self.timeout = config.getfloat('timeout', 2., above=0.)
@@ -29,9 +30,14 @@ class ShellCommand:
         if self.proc_fd is None:
             return
         try:
+            # Nutze os.read fuer BusyBox Kompatibilitaet
             data = os.read(self.proc_fd, 4096)
         except Exception:
-            pass
+            return
+        
+        if not data:
+            return
+
         data = self.partial_output + data.decode()
         if '\n' not in data:
             self.partial_output = data
@@ -47,6 +53,7 @@ class ShellCommand:
     cmd_RUN_SHELL_COMMAND_help = "Run a linux shell command"
     def cmd_RUN_SHELL_COMMAND(self, params):
         gcode_params = params.get('PARAMS','')
+        # shlex.split hilft hier enorm bei Pfaden mit Leerzeichen (S19/...)
         gcode_params = shlex.split(gcode_params)
         reactor = self.printer.get_reactor()
         try:
@@ -56,10 +63,13 @@ class ShellCommand:
             logging.exception(
                 "shell_command: Command {%s} failed" % (self.name))
             raise self.gcode.error("Error running command {%s}" % (self.name))
+        
+        hdl = None
         if self.verbose:
             self.proc_fd = proc.stdout.fileno()
             self.gcode.respond_info("Running Command {%s}...:" % (self.name))
             hdl = reactor.register_fd(self.proc_fd, self._process_output)
+        
         eventtime = reactor.monotonic()
         endtime = eventtime + self.timeout
         complete = False
@@ -68,8 +78,10 @@ class ShellCommand:
             if proc.poll() is not None:
                 complete = True
                 break
+        
         if not complete:
             proc.terminate()
+        
         if self.verbose:
             if self.partial_output:
                 self.gcode.respond_info(self.partial_output)
@@ -79,7 +91,8 @@ class ShellCommand:
             else:
                 msg = "Command {%s} timed out" % (self.name)
             self.gcode.respond_info(msg)
-            reactor.unregister_fd(hdl)
+            if hdl:
+                reactor.unregister_fd(hdl)
             self.proc_fd = None
 
 
